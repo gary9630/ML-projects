8d8f90602ea98ba928926ca9f81de0dc05360183
白振鴻 <Paichenhung@baizhenhongde-MacBook-Pro.local>
2019-08-30 18:31:19 +0800

    first week homework assignment

---
+++ 01_image_classifier/README.md
@@ -0,0 +1,4 @@
+# Image classifer using TensorFlow.js
+
+
+This project follows the [toturial](https://www.tensorflow.org/js/tutorials/transfer/image_classification) of TensorFlow.js to build a web app.
---
+++ 01_image_classifier/hello.py
@@ -0,0 +1,41 @@
+from flask import Flask
+from keras.models import load_model
+from keras.applications.resnet50 import ResNet50
+
+app = Flask(__name__)
+
+
+MODEL_PATH = 'mdoels/your_model.h5'
+
+model = ResNet50(weights='imagenet')
+
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+def model_predict(img_path, model):
+
+    img = image.load_img(img_path)
+
+    x = image.img_to_array(img)
+
+    preds = model.predict(x)
+
+    return preds
+    
+
+@app.route('/predict')
+def upload():
+    if request.method == 'POST':
+
+        f = request.files['file']
+
+        f.save(file_path)
+
+        preds = model_predict(file_path, model)
+
+        pred_class = decode_prediction(preds, topk=1)
+
+        return pred_class
